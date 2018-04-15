e7504f541980d71a3d6bf2f207e7c2797139605c
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-04-15 12:12:36 +0300

    Add JavaDoc to Calculator.java

--- src/main/java/com/eugenenuke/calculator/Calculator.java
+++ src/main/java/com/eugenenuke/calculator/Calculator.java
@@ -2,6 +2,9 @@ package com.eugenenuke.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Implement summarization
+ */
 @Service
 public class Calculator {
 	int sum(int a, int b) {
